9b8a394670da1f07346633837670a868039f377c
KimJacobsen <157890151+JacobsenKim@users.noreply.github.com>
2024-02-24 09:07:38 +0100

    Update csv_split.sh
    Signed-off-by: KimJacobsen <157890151+JacobsenKim@users.noreply.github.com>

--- py/csv_split.sh
+++ py/csv_split.sh
@@ -7,23 +7,26 @@
 ## Run like: 
 # ./csv_split.sh youre_csv_file_to_split.csv 
 #
-
-
 input_file="$1"
-split_prefix="${input_file%.*}_split_"
-split_directory="$HOME/Hentet/HA_mariaDB/output/split/"
+input_file_name="${input_file##*/}"
+split_prefix="${input_file_name%.*}_split_"
+split_directory="$HOME/Hentet/HA_mariaDB/output/split_${input_file_name%.*}/"
 
 # Check if the split directory exists, create it if it doesn't
 if [ ! -d "$split_directory" ]; then
     mkdir -p "$split_directory"
 fi
 
-# Split the input file into smaller files
-split -a 2 -d -l 10000 "$input_file" "${split_directory}${split_prefix}"
+# Split the input file into smaller files within the split folder
+split -a 3 -d -l 10000 "$input_file" "${split_directory}/${split_prefix}"
 
 # Rename the split files with the desired format
 counter=0
-for file in "${split_directory}${split_prefix}"*; do
-    mv "$file" "${split_directory}$(printf '%02d' "$counter")_${input_file%.*}_split.csv"
+for file in "${split_directory}/${split_prefix}"*; do
+    mv "$file" "${split_directory}/$(printf '%02d' "$counter")_${input_file_name%.*}_split.csv"
     ((counter++))
 done
+
+echo "############################################"
+echo "All good, it is split. Look at $split_directory for youre split csv files"
+echo "############################################"
